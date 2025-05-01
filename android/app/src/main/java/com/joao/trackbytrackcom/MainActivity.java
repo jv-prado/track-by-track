a3442c5e3b3f41f7bf1c3ca8ca3a638935f39a24
Prado <jv_prado@outlook.com>
2025-05-01 11:59:27 -0300

    implementa o android build

--- android/app/src/main/java/com/joao/trackbytrackcom/MainActivity.java
+++ android/app/src/main/java/com/joao/trackbytrackcom/MainActivity.java
@@ -2,6 +2,4 @@ package com.joao.trackbytrackcom;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    // Permitir que o Capacitor lide com o botão voltar por padrão
-}
+public class MainActivity extends BridgeActivity {}
